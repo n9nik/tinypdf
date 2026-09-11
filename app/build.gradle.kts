1532c20d71e879675019c3bbf5f71a927c95d223
n9nik <nikhil.suresh@gmail.com>
2026-09-10 23:00:04 -0700

    Bump versionCode to 2 for Play Alpha re-upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.n9nik.pdftools"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         manifestPlaceholders["admobAppId"] = configuredAdMobAppId.get()
